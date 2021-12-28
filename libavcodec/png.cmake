b9e3a6a67cd767400b21a51917ba78d80ca49d42
Pawday <pawday@mail.ru>
2021-12-28 15:32:24 +0300

    [cmake] new include strategy (CMAKE_BINARY_DIR -> CMAKE_CURRENT_BINARY_DIR)

--- libavcodec/png.cmake
+++ libavcodec/png.cmake
@@ -1,6 +1,7 @@
 option(ZLIB_SOURCE_DIR "ZLIB source directory" OFF)
 
 if (NOT ZLIB_SOURCE_DIR)
+    #this file is including to libavcodec/CMakeLists.txt when png is registered
     message(FATAL_ERROR "For png codec you MUST specify ZLIB_SOURCE_DIR option")
 endif ()
 
@@ -18,5 +19,7 @@ list(APPEND AVCODEC_REGISTERED_CODECS_SOURCES png.c)
 list(APPEND AVCODEC_REGISTERED_CODECS_SOURCES pngenc.c)
 list(APPEND AVCODEC_REGISTERED_CODECS_SOURCES pngdec.c)
 list(APPEND AVCODEC_REGISTERED_CODECS_SOURCES pngdsp.c)
+
+list(APPEND AVCODEC_REGISTERED_CODECS_SOURCES png_parser.c)
 
 list(APPEND AVCODEC_REGISTERED_CODECS_SOURCES lossless_videoencdsp.c)
